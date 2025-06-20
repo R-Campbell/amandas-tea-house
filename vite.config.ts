b542774fa9979b184955b1daf89d14f2035bc916
Rob Campbell <robertcharles.campbell@gmail.com>
2025-06-20 10:06:58 -0400

    use root path

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig(({ command }) => ({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: command === 'serve' ? '/' : '/amandas-tea-house/'
+  base: '/'
 }))
